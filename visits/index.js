49156ccb8dbe81b24d548a61e318393ee0cb1c39
ksanjaykumar1 <ksanjayk@protonmail.com>
2021-05-14 19:03:56 +0530

    node process exit with 0, restart policies with always

--- visits/index.js
+++ visits/index.js
@@ -1,5 +1,6 @@
 const express = require("express")
 const redis = require('redis')
+const process = require('process')
 const app = express();
 const client = redis.createClient({
     host:'redis-server',
@@ -8,6 +9,8 @@ const client = redis.createClient({
 client.set('visits',0)
 
 app.get('/',(req,res)=>{
+    //we manually exited from the process
+    process.exit(0)
     client.get('visits',(err,visits)=>{
         res.send('Number of visits is'+ visits);
         client.set('visits',parseInt(visits)+1)
